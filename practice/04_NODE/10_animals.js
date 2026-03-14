6c4081a1d7480477f9aac2bad4fc466478816f2f
junsoo0719 <junsoo0719@gmail.com>
2026-03-14 11:26:59 +0900

    2026-03-13 강의 예제: 자바스크립트 기초 문법과 모듈 예제 정리

---
+++ practice/04_NODE/10_animals.js
@@ -0,0 +1,10 @@
+const animals = ['dog', 'cat'];
+
+function showAnimals() {
+  animals.map((item) => console.log(item));
+}
+
+module.exports = {
+  animals,
+  showAnimals,
+};
---
+++ practice/04_NODE/12_animals-es6.js
@@ -0,0 +1,5 @@
+export const animals = ['dog', 'cat'];
+
+export function showAnimals() {
+  animals.map((item) => console.log(item));
+}
