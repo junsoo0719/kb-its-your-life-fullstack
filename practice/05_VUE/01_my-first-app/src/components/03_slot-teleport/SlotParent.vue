95e2d836d89be73cad734157cfed2398fdf576c3
junsoo0719 <junsoo0719@gmail.com>
2026-03-26 15:48:17 +0900

    2026-03-25 강의 예제: Slot Teleport 예제 정리

---
+++ practice/05_VUE/01_my-first-app/src/components/03_slot-teleport/SlotParent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <SlotChild>
+      <h1>가냐?</h1>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_UafMmRvEgZFR2QvA0MGHZj6jCFlspiEiPg&s"
+      />
+    </SlotChild>
+  </div>
+</template>
+
+<script>
+import SlotChild from './SlotChild.vue';
+
+export default {
+  components: {
+    SlotChild,
+  },
+};
+</script>
