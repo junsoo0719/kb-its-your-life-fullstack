a70e8a29397ff24dae40fc330e965bba434196f2
junsoo0719 <junsoo0719@gmail.com>
2026-03-28 23:47:38 +0900

    2026-03-24 ch07 실습: 단일 파일 컴포넌트를 이용한 Vue 애플리케이션 개발 예제

---
+++ practice/05_VUE/ch07/todolist-app/src/components/InputTodo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    할 일
+    <input
+      type="text"
+      class="form-control"
+      v-mode.trim="text"
+      @keyup.enter="addTodoHandler"
+      placeholder="할일을 여기에 입력!"
+    />
+    <span class="btn btn-primary input-group-addon" @click="addTodoHandler"
+      >추가</span
+    >
+  </div>
+</template>
+<script>
+export default {
+  name: 'InputTodo',
+  data() {
+    return {
+      text: '',
+    };
+  },
+  emits: ['add-todo'],
+  methods: {
+    addTodoHandler() {
+      if (this.text.length >= 2) {
+        this.$emit('add-todo', this.text);
+        this.text = '';
+      }
+    },
+  },
+};
+</script>
