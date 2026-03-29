4c1fb346ab7b4f1525f08d91794b77c762e383fb
junsoo0719 <junsoo0719@gmail.com>
2026-03-30 01:39:32 +0900

    2026-03-27 ch09 이론: Composition API 정리

---
+++ theory/03_VUE/ch09/calc-component-test/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Calc />
+  </div>
+</template>
+<script>
+import Calc from './components/Calc8.vue';
+export default {
+  name: 'App',
+  components: { Calc },
+};
+</script>
