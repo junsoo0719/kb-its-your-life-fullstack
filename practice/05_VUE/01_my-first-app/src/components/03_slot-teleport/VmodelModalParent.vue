a10f7767b355e19b155f8fc6d9a5f4d7bf1164da
junsoo0719 <junsoo0719@gmail.com>
2026-03-30 16:53:27 +0900

    2026-03-25 VUE 강의 예제: Slot Teleport 예제 정리

---
+++ practice/05_VUE/01_my-first-app/src/components/03_slot-teleport/VmodelModalParent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button @click="isModalVisible = true">모달 열기</button>
+    <div class="content">
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+      <h1>원래 부모 요소의 컨텐츠 입니다!!!</h1>
+    </div>
+    <ModalChild v-model:visible="isModalVisible">
+      <h2>이 모달을 당장 닫지 않으면!?</h2>
+      <p>심쿵할 겁니다!</p>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQrk0i8zr26BNSU_q6HodebyCpZkEIf6VB14ilipceGPYrtRG9qhvDscZWj71VuT5-eVBY9igsT8BppkNz6NYmuwPpOrapa_7fklZkhvsXmxRk3uZZgq7DrqpfOhG3mIJdoNEMHIis11Ks&s=19"
+      />
+    </ModalChild>
+  </div>
+</template>
+
+<script>
+import ModalChild from './ModalChild.vue';
+
+export default {
+  components: {
+    ModalChild,
+  },
+  data() {
+    return {
+      isModalVisible: false,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
