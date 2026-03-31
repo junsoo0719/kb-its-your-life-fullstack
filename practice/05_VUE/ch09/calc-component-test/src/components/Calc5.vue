b8c123205d40e5ee5f9bca84a44ac60e3f3e7f25
junsoo0719 <junsoo0719@gmail.com>
2026-03-31 11:25:47 +0900

    2026-03-27 실습: Composition API 예제

---
+++ practice/05_VUE/ch09/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    x : <input type="text" v-model.number="x" />
+    <br />
+    결과 : {{ result }}
+  </div>
+</template>
+<script>
+import { ref, watch } from 'vue';
+export default {
+  name: 'Calc5',
+  setup() {
+    const x = ref(0); // 0으로 초기화
+    const result = ref(0); // 0으로 초기화
+    watch(x, (cur, old) => {
+      console.log(`${old} -> ${cur}`);
+      result.value = 2 * cur;
+    });
+    return { x, result };
+  },
+};
+</script>
