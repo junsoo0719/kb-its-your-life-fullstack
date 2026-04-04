6bbc207f6f494ded461e6ab7896ea3006cdd7846
junsoo0719 <junsoo0719@gmail.com>
2026-04-04 23:56:32 +0900

    2026-04-03 ch12 이론: vue-router와 axios를 사용한 예제 정리

---
+++ theory/03_VUE/ch12/todolist-app-router/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import About from '@/pages/About.vue';
+import TodoList from '@/pages/TodoList.vue';
+import AddTodo from '@/pages/AddTodo.vue';
+import EditTodo from '@/pages/EditTodo.vue';
+import NotFound from '@/pages/NotFound.vue';
+import Home from '@/pages/Home.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    // { path: '/', component: Home },
+    { path: '/', redirect: '/todos' },
+    { path: '/about', component: About },
+    { path: '/todos', component: TodoList },
+    { path: '/todos/add', component: AddTodo },
+    { path: '/todos/edit/:id', component: EditTodo },
+    { path: '/:paths(.*)*', component: NotFound },
+  ],
+});
+
+export default router;
