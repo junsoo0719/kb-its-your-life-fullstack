57df1edc880976ae7d4e389e661b2517b7da8109
junsoo0719 <junsoo0719@gmail.com>
2026-04-06 15:31:11 +0900

    2026-04-01 VUE 강의 예제: vue-router-app 예제 정리

---
+++ practice/05_VUE/02_vue-router-app/src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+  <div>
+    <h1>404 Not Found</h1>
+    <img
+      src="https://biz.chosun.com/resizer/v2/ITX6BGQ5LYVWC2YIYCMOLSAB5E.jpg?auth=d6a39af5925462230a21c35f0a2c31ce133496c11df064895572b38b18aa2f2a&width=464"
+      alt="없어요"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
