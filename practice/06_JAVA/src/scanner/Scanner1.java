77f8b6d365634c719b01fb6f29cdaae4d32f6e71
junsoo0719 <junsoo0719@gmail.com>
2026-04-16 01:35:47 +0900

    2026-04-15 강의 예제: scanner 예제 정리

---
+++ practice/06_JAVA/src/scanner/Scanner1.java
@@ -0,0 +1,21 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("문자열을 입력하세요 : ");
+        String str = scanner.nextLine();
+        System.out.println("입력한 문자열은!? : " + str);
+
+        System.out.println("정수를 입력하세요 : ");
+        int intNum = scanner.nextInt();
+        System.out.println("입력한 정수는!? : " + intNum);
+
+        System.out.println("실수를 입력하세요 : ");
+        double doubleNum = scanner.nextDouble();
+        System.out.println("입력한 실수는!? : " + doubleNum);
+    }
+}
---
+++ practice/06_JAVA/src/scanner/ScannerEx1.java
@@ -0,0 +1,24 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerEx1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int randomNum = (int) (Math.random() * 100);
+
+        while (true) {
+            System.out.print("랜덤 숫자를 입력하세요 : ");
+            int input = scanner.nextInt();
+
+            if (input > randomNum) {
+                System.out.println("Down");
+            } else if (input < randomNum) {
+                System.out.println("Up");
+            } else {
+                System.out.println("정답입니다! 랜덤 숫자는 " + randomNum);
+                break;
+            }
+        }
+    }
+}
