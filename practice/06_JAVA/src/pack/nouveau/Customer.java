ec92056d8d9dd934c344b4f466ec65471a955a0e
junsoo0719 <junsoo0719@gmail.com>
2026-04-17 12:24:39 +0900

    2026-04-16 강의 예제: 패키지 예제 정리

---
+++ practice/06_JAVA/src/pack/nouveau/Customer.java
@@ -0,0 +1,12 @@
+package pack.nouveau;
+
+public class Customer {
+    String name = "송준수";
+    int age = 26;
+    int total = 50000;
+    boolean blacklist = false;
+
+    public Customer() {
+        System.out.println("pack.nouveau 에서 Customer 생성!!");
+    }
+}
