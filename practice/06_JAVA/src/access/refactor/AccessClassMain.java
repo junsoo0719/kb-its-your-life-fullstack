3e8a5b9133dc92a46475d2d2aaf6b82b68b7bbe0
junsoo0719 <junsoo0719@gmail.com>
2026-04-17 12:24:50 +0900

    2026-04-16 강의 예제: 접근 제어자 예제 정리

---
+++ practice/06_JAVA/src/access/refactor/AccessClassMain.java
@@ -0,0 +1,8 @@
+package access.refactor;
+
+public class AccessClassMain {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        DefaultClass defaultClass = new DefaultClass();
+    }
+}
