4cd74b64cc91c5a79bc3409870d91b505702a1d9
junsoo0719 <junsoo0719@gmail.com>
2026-04-19 23:41:00 +0900

    2026-04-17 강의 예제: Memory_Static 예제 정리

---
+++ practice/06_JAVA/src/memory/AddStrMain.java
@@ -0,0 +1,9 @@
+package memory;
+
+public class AddStrMain {
+    public static void main(String[] args) {
+        String str1 = "내일 이효석 강사의 정처기는 ";
+        String result = AddStr.addStr(str1);
+        System.out.println(result);
+    }
+}
