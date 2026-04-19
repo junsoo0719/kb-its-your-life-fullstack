ccbf4971c199047d0f46f44be3b02670cc139602
junsoo0719 <junsoo0719@gmail.com>
2026-04-19 19:50:17 +0900

    2026-04-03 ch10 실습: 라우트와 Axios 연동 예제

---
+++ practice/05_VUE/ch10/router-test/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Header />
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: { Header },
+};
+</script>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 10px;
+}
+</style>
