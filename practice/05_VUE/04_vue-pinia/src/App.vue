8447ee055c7d7d4c0bda4f8b2b41ea504d469ad7
junsoo0719 <junsoo0719@gmail.com>
2026-04-19 19:51:22 +0900

    2026-04-06 VUE 강의 예제: pinia 예제 정리

---
+++ practice/05_VUE/04_vue-pinia/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Counter1 from './components/Counter1.vue';
+import Counter2 from './components/Counter2.vue';
+import Counter3 from './components/Counter3.vue';
+</script>
+
+<template>
+  <!-- 서로 다른 컴포넌트이지만 같은 pinia store의 값을 사용함 -->
+  <Counter1 />
+  <Counter2 />
+  <Counter3 />
+</template>
+
+<style scoped></style>
---
+++ practice/05_VUE/04_vue-pinia/src/components/Counter3.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+
+const counterStore = useCounterStore();
+
+const { count, doubleCount } = storeToRefs(counterStore);
+const { increment, decrement } = counterStore;
+</script>
+
+<template>
+  <div>
+    <h1>Counter</h1>
+    <h2>count : {{ count }}</h2>
+    <h2>count x 2 : {{ doubleCount }}</h2>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
