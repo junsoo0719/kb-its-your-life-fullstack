da2b4ed33e3f243cbc956239588dfb70b18eb5df
junsoo0719 <junsoo0719@gmail.com>
2026-04-20 09:21:46 +0900

    2026-04-16 강의 예제: 클래스 예제 정리

---
+++ practice/06_JAVA/src/access/SpeakerMain.java
@@ -0,0 +1,13 @@
+package access;
+
+public class SpeakerMain {
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker();
+
+        speaker.volumeUp();
+        speaker.volumeUp();
+        speaker.volumeDown();
+        speaker.volumeDown();
+        speaker.volumeDown();
+    }
+}
