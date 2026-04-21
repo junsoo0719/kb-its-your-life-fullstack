0122927028a533c03d445d34827ce81674f082fe
junsoo0719 <junsoo0719@gmail.com>
2026-04-21 11:43:32 +0900

    2026-04-20 ch07 이론: 상속 정리

--- theory/04_JAVA/src/ch07/sec10/exam02/AbstractMethodExample.java
+++ theory/04_JAVA/src/ch07/sec10/exam02/AbstractMethodExample.java
@@ -1,4 +1,19 @@
 package ch07.sec10.exam02;
 
 public class AbstractMethodExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.sound();
+
+        Cat cat = new Cat();
+        cat.sound();
+
+        //매개변수의 다형성
+        animalSound(new Dog());
+        animalSound(new Cat());
+    }
+
+    private static void animalSound(Animal animal) {
+        animal.sound(); // 재정의된 메소드 호출
+    }
 }
