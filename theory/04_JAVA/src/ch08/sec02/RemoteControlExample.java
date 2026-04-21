b62ec9c82d3ae21964875c5d953f70d02761f6f8
junsoo0719 <junsoo0719@gmail.com>
2026-04-21 11:43:55 +0900

    2026-04-20 ch08 이론: 인터페이스 정리

--- theory/04_JAVA/src/ch08/sec02/RemoteControlExample.java
+++ theory/04_JAVA/src/ch08/sec02/RemoteControlExample.java
@@ -1,4 +1,30 @@
 package ch08.sec02;
 
 public class RemoteControlExample {
+    public static void main(String[] args) {
+        //인터페이스 변수 선언
+        RemoteControl rc;
+
+        rc = new Television(); // 기존 코드
+        rc.turnOn();
+        rc.setVolume(5);
+
+        //디폴트 메소드 호출
+        rc.setMute(true);
+        rc.setMute(false);
+
+        System.out.println();
+
+        rc = new Audio(); // setMute 재정의 코드
+        rc.turnOn();
+        rc.setVolume(5);
+
+        //디폴트 메소드 호출
+        rc.setMute(true);
+        rc.setMute(false);
+
+        //정적 메소드 호출
+        RemoteControl.changeBattery();
+    }
 }
+
