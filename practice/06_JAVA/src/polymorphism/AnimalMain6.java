2cb7445886189c5f79b29f866f93fc856c9c5c7d
junsoo0719 <junsoo0719@gmail.com>
2026-04-25 18:47:55 +0900

    2026-04-15 강의 예제: 다형성 예제 정리

---
+++ practice/06_JAVA/src/polymorphism/AnimalMain6.java
@@ -0,0 +1,33 @@
+package polymorphism;
+
+public class AnimalMain6 {
+    public static void main(String[] args) {
+        Animal donkey = new Donkey();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        Animal chicken = new Chicken();
+
+//        donkey.sound();
+//        dog.sound();
+//        cat.sound();
+//        chicken.sound();
+
+//        soundAnimal(donkey);
+//        soundAnimal(dog);
+//        soundAnimal(cat);
+//        soundAnimal(chicken);
+        Animal[] animals = {new Donkey(), new Dog(), new Cat(), new Chicken()};
+        for (Animal animal : animals) {
+            soundAnimal(animal);
+        }
+    }
+
+    private static void soundAnimal(Animal animal) {
+        animal.sound();
+
+        if (animal instanceof Cat) {
+            Cat poly = (Cat) animal;
+            poly.grooming();
+        }
+    }
+}
