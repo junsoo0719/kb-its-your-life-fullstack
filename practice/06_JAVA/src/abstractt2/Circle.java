a73f39ce1e50c4a016c40dc9bec5b5b86b9f0366
junsoo0719 <junsoo0719@gmail.com>
2026-04-28 16:50:41 +0900

    2026-04-21 강의 예제: AbstractClass 추상 메서드 상속 및 사용 예제 정리

---
+++ practice/06_JAVA/src/abstractt2/Circle.java
@@ -0,0 +1,21 @@
+package abstractt2;
+
+public class Circle extends Shape {
+    private double radius;
+    private static final double PI = Math.PI;
+
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * PI * radius;
+    }
+}
