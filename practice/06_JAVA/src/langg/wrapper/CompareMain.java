19f66c33a8842e065f3129dd4afa0ee4dcbcd0ac
junsoo0719 <junsoo0719@gmail.com>
2026-04-28 16:41:33 +0900

    2026-04-23 강의 예제: Wrapper 클래스 예제 정리

---
+++ practice/06_JAVA/src/langg/wrapper/CompareMain.java
@@ -0,0 +1,20 @@
+package langg.wrapper;
+
+public class CompareMain {
+    public static void main(String[] args) {
+        Integer int1 = Integer.valueOf(10);
+        Long long1 = 100L;
+        Double double1 = 11.18;
+
+        int intValue = int1;
+        long longValue = long1;
+        double doubleValue = double1;
+
+        System.out.println("intValue = " + intValue);
+        System.out.println("int1 = " + int1);
+        System.out.println("longValue = " + longValue);
+        System.out.println("long1 = " + long1);
+        System.out.println("doubleValue = " + doubleValue);
+        System.out.println("double1 = " + double1);
+    }
+}
