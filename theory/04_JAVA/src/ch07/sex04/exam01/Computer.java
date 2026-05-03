4ce07533342e1e416d460341bbd8095adf7a9bfc
junsoo0719 <junsoo0719@gmail.com>
2026-05-04 01:13:00 +0900

    2026-04-17 ch07 이론: 상속 정리

--- theory/04_JAVA/src/ch07/sex04/exam01/Computer.java
+++ theory/04_JAVA/src/ch07/sex04/exam01/Computer.java
@@ -1,4 +1,10 @@
 package ch07.sex04.exam01;
 
-public class Computer {
+public class Computer extends Calculator{
+    // 메소드 오버라이딩
+
+    @Override
+    public double areaCircle(double r) {
+        return super.areaCircle(r);
+    }
 }
