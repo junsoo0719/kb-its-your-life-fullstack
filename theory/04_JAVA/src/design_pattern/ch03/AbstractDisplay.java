582bac7275e4c9376c07e8811d6f25a0959e45f8
junsoo0719 <junsoo0719@gmail.com>
2026-05-04 01:24:27 +0900

    2026-04-21 ch03 이론: Template Method 정리

--- theory/04_JAVA/src/design_pattern/ch03/AbstractDisplay.java
+++ theory/04_JAVA/src/design_pattern/ch03/AbstractDisplay.java
@@ -1,4 +1,21 @@
 package design_pattern.ch03;
 
-public class AbstractDisplay {
+public abstract class AbstractDisplay {
+    public abstract void open();
+
+    public abstract void print();
+
+    public abstract void close();
+
+    // AbstractDisplay에서 구현하는 메서드(템플릿)
+    public final void display() {
+        open();
+        for(int
+            i = 0;
+            i < 5;
+            i++) {
+            print();
+        }
+        close();
+    }
 }
--- theory/04_JAVA/src/design_pattern/ch03/StringDisplay.java
+++ theory/04_JAVA/src/design_pattern/ch03/StringDisplay.java
@@ -1,4 +1,34 @@
 package design_pattern.ch03;
 
-public class StringDisplay {
+public class StringDisplay extends AbstractDisplay {
+    private String string; // 표시해야 하는 문자열
+    private int width; // 문자열의 길이
+
+    public StringDisplay(String string) {
+        this.string = string;
+        this.width = string.length();
+    }
+
+    @Override
+    public void open() {
+        printLine();
+    }
+
+    @Override
+    public void print() {
+        System.out.println("|" + string + "|");
+    }
+
+    @Override
+    public void close() {
+        printLine();
+    }
+
+    private void printLine() {
+        System.out.print("+");
+        for (int i = 0; i < width; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
 }
