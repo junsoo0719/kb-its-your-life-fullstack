84564433c48368f1995faa0fd1def36b0f5c10bb
junsoo0719 <junsoo0719@gmail.com>
2026-06-18 16:43:44 +0900

    2026-06-15 Spring_Context 이론: 스프링 컨텍스트 - 빈 정의 정리

---
+++ theory/08_Spring/Spring_Context/ex01/src/main/java/app/Main.java
@@ -0,0 +1,21 @@
+package app;
+
+import config.ProjectConfig;
+import domain.Parrot;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer n = context.getBean(Integer.class);
+        System.out.println(n);
+    }
+}
---
+++ theory/08_Spring/Spring_Context/ex01/src/main/java/config/ProjectConfig3.java
@@ -0,0 +1,12 @@
+package config;
+
+import domain.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "domain")
+public class ProjectConfig3 {
+
+}
