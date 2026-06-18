08f067bed92924e9a3defd9b0f54c633218ec4db
junsoo0719 <junsoo0719@gmail.com>
2026-06-18 16:44:41 +0900

    2026-06-15 Spring_Context 이론: 개발을 위한 준비 정리

---
+++ theory/08_Spring/SpringLegacy/src/main/java/org/scoula/ex01/config/RootConfig.java
@@ -0,0 +1,7 @@
+package org.scoula.ex01.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+}
---
+++ theory/08_Spring/SpringLegacy/src/main/java/org/scoula/ex01/config/ServletConfig.java
@@ -0,0 +1,33 @@
+package org.scoula.ex01.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "org.scoula.ex01.controller")
+public class ServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**") // url이 /resources/로 시작하는 모든 경로
+                .addResourceLocations("/resources/"); // webapp/resources/경로로 매핑
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/views/");
+        bean.setSuffix(".jsp");
+
+        registry.viewResolver(bean);
+
+    }
+}
