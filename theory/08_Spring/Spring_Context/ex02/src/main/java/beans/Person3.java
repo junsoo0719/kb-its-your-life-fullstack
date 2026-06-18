804720b9a7850aa4bb6ab6fff105fc35f5e22f7e
junsoo0719 <junsoo0719@gmail.com>
2026-06-18 16:44:08 +0900

    2026-06-15 Spring_Context 이론: 스프링 컨텍스트 - 빈 작성 정리

---
+++ theory/08_Spring/Spring_Context/ex02/src/main/java/beans/Person3.java
@@ -0,0 +1,32 @@
+package beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person3 {
+    private String name = "Ella";
+
+    private final Parrot2 parrot;
+
+    @Autowired
+    public Person3(Parrot2 parrot) {
+        this.parrot = parrot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Parrot2 getParrot() {
+        return parrot;
+    }
+
+    public void setParrot(Parrot2 parrot) {
+        this.parrot = parrot;
+    }
+}
